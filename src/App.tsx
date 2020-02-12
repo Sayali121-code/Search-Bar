97533012bc7a1e922b13d568a5caa4d0785014f4
Sayali More <sayali.more@coditas.com>
2020-02-12 13:22:59 +0530

    React Search Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from "react";
 import './App.css';
+import axios from 'axios';
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    camps :[],
+    search: ""
+  }
+
+
+  componentDidMount() {
+    axios.get('http://13.71.86.210/api/v1/getCamps')
+    .then(res => {
+      console.log('>> res', res);
+      const camps = res.data.camps;
+      this.setState({ camps });
+    })
+  }
+
+  enteredText=(event:any) => {
+  this.setState({
+    search:event.target.value
+  })
+  }
+
+  render() {
+   // console.log(this.state.search)
+   
+    return (
+      <div>
+        <form>
+          <input 
+          type="text" 
+          placeholder="Search" 
+          name="camp_name"
+          value={this.state.search}
+          onChange={this.enteredText}
+          /> 
+        </form>
+
+     {/*   {this.state.camps.map((data: any,i,arr) => {
+          return (
+          <div key={i}>
+            {data.campVenue}  -  {data.territoryName} 
+          </div> )
+        }
+      )} */}
+
+    {/*   {this.state.camps.map((data: any,i,arr) => {
+        if(this.state.search === data.territoryName) 
+        return (
+          <div key={i}>
+            {data.campVenue} - {data.territoryName}
+          </div> 
+          )
+       }
+      )} */}
+      
+     {this.state.camps.map((data: any,i,arr) => {
+        if(data.territoryName.toLowerCase().includes(this.state.search.toLowerCase()) || data.campVenue.toLowerCase().includes(this.state.search.toLowerCase())) 
+        return (
+          <div key={i}>
+            {data.campVenue} - {data.territoryName}
+          </div> 
+          )
+       }
+      )} 
+      </div>
+    )
+  }
 }
 
 export default App;
